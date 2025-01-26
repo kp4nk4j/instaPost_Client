f46efd8aefcd2fd2084cc3655c7d64e54241efae
kp4nk4j <pankajkvs2001@gmail.com>
2025-01-26 12:48:00 +0530

    Change URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'https://instapost-server.up.railway.app/posts'
+const url = 'https://instapostserver.up.railway.app/posts'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
